6f855db21401d458569b32e8de3007b7baadeed6
Debayo Tomisin <debayotomisin07@gmail.com>
2021-03-03 11:11:42 -0800

    root css folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from "./components/Header/Header"
+
 function App() {
   return (
     <>
+      <Header/>
+      <br/>
+      <br/>
       <h1>This is the begining boys</h1>
     </>
   );
